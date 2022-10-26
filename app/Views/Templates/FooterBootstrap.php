4a5f253d816f9c07b00223aafba36c9499fd82b3
Risma Nur <rismanur252@gmail.com>
2022-10-26 11:19:44 +0700

    push coba

--- app/Views/Templates/FooterBootstrap.php
+++ app/Views/Templates/FooterBootstrap.php
@@ -1,11 +1,11 @@
 	<!--start overlay-->
-    <div class="overlay toggle-icon"></div>
-		<!--end overlay-->
-		<!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
-		<!--End Back To Top Button-->
-		<footer class="page-footer">
-			<p class="mb-0">Copyright © 2021. All right reserved.</p>
-		</footer>
+	<div class="overlay toggle-icon"></div>
+	<!--end overlay-->
+	<!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+	<!--End Back To Top Button-->
+	<footer class="page-footer">
+		<p class="mb-0">Copyright © 2021. All right reserved.</p>
+	</footer>
 	</div>
 	<!--end wrapper-->
 	<!--start switcher-->
@@ -17,9 +17,9 @@
 				<h5 class="mb-0 text-uppercase">Theme Customizer</h5>
 				<button type="button" class="btn-close ms-auto close-switcher" aria-label="Close"></button>
 			</div>
-			<hr/>
+			<hr />
 			<h6 class="mb-0">Theme Styles</h6>
-			<hr/>
+			<hr />
 			<div class="d-flex align-items-center justify-content-between">
 				<div class="form-check">
 					<input class="form-check-input" type="radio" name="flexRadioDefault" id="lightmode">
@@ -34,14 +34,14 @@
 					<label class="form-check-label" for="semidark">Semi Dark</label>
 				</div>
 			</div>
-			<hr/>
+			<hr />
 			<div class="form-check">
 				<input class="form-check-input" type="radio" id="minimaltheme" name="flexRadioDefault">
 				<label class="form-check-label" for="minimaltheme">Minimal Theme</label>
 			</div>
-			<hr/>
+			<hr />
 			<h6 class="mb-0">Header Colors</h6>
-			<hr/>
+			<hr />
 			<div class="header-colors-indigators">
 				<div class="row row-cols-auto g-3">
 					<div class="col">
@@ -70,9 +70,9 @@
 					</div>
 				</div>
 			</div>
-			<hr/>
+			<hr />
 			<h6 class="mb-0">Sidebar Colors</h6>
-			<hr/>
+			<hr />
 			<div class="header-colors-indigators">
 				<div class="row row-cols-auto g-3">
 					<div class="col">
@@ -105,36 +105,36 @@
 	</div>
 	<!--end switcher-->
 
-<script src="/assets/js/bootstrap.bundle.min.js"></script>
+	<script src="/assets/js/bootstrap.bundle.min.js"></script>
 	<!--plugins-->
 	<script src="/assets/js/jquery.min.js"></script>
 	<script src="/assets/plugins/simplebar/js/simplebar.min.js"></script>
 	<script src="/assets/plugins/metismenu/js/metisMenu.min.js"></script>
 	<script src="/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
 	<script src="/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+	<script src="/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
 	<script src="/assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
 	<script src="/assets/plugins/chartjs/js/Chart.min.js"></script>
 	<script src="/assets/plugins/chartjs/js/Chart.extension.js"></script>
 	<script src="/assets/js/index2.js"></script>
 	<!--app JS-->
 	<script src="/assets/js/app.js"></script>
-    <script src="/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+	<script src="/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
 	<script src="/assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
 
-    <script>
+	<script>
 		$(document).ready(function() {
 			$('#example').DataTable();
-		  } );
+		});
 	</script>
 	<script>
 		$(document).ready(function() {
-			var table = $('#example2').DataTable( {
+			var table = $('#example2').DataTable({
 				lengthChange: false,
-				buttons: [ 'copy', 'excel', 'pdf', 'print']
-			} );
-		 
+				buttons: ['copy', 'excel', 'pdf', 'print']
+			});
+
 			table.buttons().container()
-				.appendTo( '#example2_wrapper .col-md-6:eq(0)' );
-		} );
+				.appendTo('#example2_wrapper .col-md-6:eq(0)');
+		});
 	</script>
