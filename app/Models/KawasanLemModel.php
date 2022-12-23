635294f8843dbbab29773cca574d2b25c9b5f1cc
Nada <nadahitna23@gmail.com>
2022-12-23 11:20:52 +0700

    rename

--- app/Models/KawasanLemModel.php
+++ app/Models/KawasanLemModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class KawasanModel extends Model
+class KawasanLemModel extends Model
 {
     // atribut tabel diisi dengan nama tabel
     protected $table = 'tbl_kawasan';
